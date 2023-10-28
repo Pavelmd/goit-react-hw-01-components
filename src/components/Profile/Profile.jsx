284daa079aded0d42e3306bf9a509909388c3a0e
Pavelmd <pashanovosvit2@gmail.com>
2023-10-28 19:46:41 +0300

    add profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+// import css from './Profile.module.css';
+
+export const Profile = ({ avatar, username, tag, location, stats }) => (
+  <div>
+    <div>
+      <img src={avatar} alt="User avatar"  />
+      <p>{username}</p>
+      <p>@{tag}</p>
+      <p>{location}</p>
+    </div>
+
+    <ul>
+      <li>
+        <span>Followers</span>
+        <span>{stats.followers}</span>
+      </li>
+      <li>
+        <span>Views</span>
+        <span>{stats.views}</span>
+      </li>
+      <li>
+        <span>Likes</span>
+        <span>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
+
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
